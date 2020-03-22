0a5d75a4a690f3028b96bac78eda0dbe120a760d
nachoMorera <ijmorera19@gmail.com>
2020-03-22 18:34:47 +0100

    commit aunque sigue sin ir 2

---
+++ app/src/main/java/com/example/workmeout/ui/notifications/BlogPost.kt
@@ -0,0 +1,13 @@
+package com.example.workmeout.ui.notifications
+
+data class BlogPost (
+    var title: String,
+
+    var body: String,
+
+    var image: String,
+
+    var username: String
+) {
+
+}
---
+++ app/src/main/java/com/example/workmeout/ui/notifications/BlogRecyclerAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.workmeout.ui.notifications
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.workmeout.R
+import kotlinx.android.synthetic.main.layout_blog_list_item.view.*
+import kotlin.collections.ArrayList
+
+class BlogRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var items: List<BlogPost> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return BlogViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.layout_blog_list_item, parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder){
+
+            is BlogViewHolder ->{
+                holder.bind(items.get(position))
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun submitList(blogList: List<BlogPost>){
+        items = blogList
+    }
+
+    class BlogViewHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val blogImage: ImageView = itemView.blog_image
+        val blogTitle:TextView = itemView.blog_title
+        val blogAuthor = itemView.blog_author
+
+        fun bind(blogPost: BlogPost) {
+            blogTitle.text = blogPost.title
+            blogAuthor.text = blogPost.username
+
+            val requestOptions = RequestOptions()
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
+
+            Glide.with(itemView.context)
+                .applyDefaultRequestOptions(requestOptions)
+                .load(blogPost.image)
+                .into(blogImage)
+        }
+    }
+
+}

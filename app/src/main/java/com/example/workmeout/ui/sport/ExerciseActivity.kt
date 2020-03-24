d05f4f663df1d757ff568f98f7e8e2c894225b5d
gerardPerello <perello999@gmail.com>
2020-03-24 21:39:28 +0100

    Garfic boi

--- app/src/main/java/com/example/workmeout/ui/sport/ExerciseActivity.kt
+++ app/src/main/java/com/example/workmeout/ui/sport/ExerciseActivity.kt
@@ -1,10 +1,15 @@
 package com.example.workmeout.ui.sport
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import androidx.core.graphics.component3
 import com.example.workmeout.R
+import com.example.workmeout.ui.MainActivity
 import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.PointsGraphSeries
 import java.util.*
@@ -17,31 +22,60 @@ class ExerciseActivity : AppCompatActivity() {
         tvName = findViewById(R.id.textExerciseName)
         var exName: String = getIntent().getStringExtra("exName")
         var exWeight: Double = getIntent().getDoubleExtra("exWeight",0.0)
-
+        val calendar: Calendar = Calendar.getInstance();
+        val today: Date = calendar.time;
         tvName.setText(exName);
         val graphView: GraphView = findViewById(R.id.grafic);
 
-        val series: PointsGraphSeries<DataPoint> = PointsGraphSeries<DataPoint>(getDataPoints(exWeight));
+        val series: PointsGraphSeries<DataPoint> = PointsGraphSeries<DataPoint>(getDataPoints(exWeight,today));
 
         graphView.addSeries(series);
+        val objj: DateAsXAxisLabelFormatter = DateAsXAxisLabelFormatter(this)
+        graphView.gridLabelRenderer.setLabelFormatter(objj)
+        graphView.gridLabelRenderer.numHorizontalLabels = 5
+
+        graphView.viewport.setMinX(sumarRestarDiasFecha(today,-4).time.toDouble())
+        graphView.viewport.setMinY(0.0)
+        graphView.viewport.setMaxX(today.time.toDouble())
+        graphView.viewport.setMaxY(100.0)
+        graphView.viewport.isXAxisBoundsManual = true
+        graphView.viewport.isYAxisBoundsManual = true
+
+        graphView.gridLabelRenderer.setHumanRounding(false)
+
     }
 
-    fun getDataPoints(args: Double): Array<DataPoint>{
-        val calendar: Calendar = Calendar.getInstance();
-        val today: Date = calendar.time;
+    fun returnButton(view: View){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
 
-        val primer: DataPoint = DataPoint(0.0,1.0)
-        val segon: DataPoint = DataPoint(1.0,2.0)
-        val tercer: DataPoint = DataPoint(2.0,3.0)
-        val quart: DataPoint = DataPoint(3.0,5.0)
-        val cinc: DataPoint = DataPoint(4.0,args)
+    fun getDataPoints(args: Double, today: Date): Array<DataPoint>{
+
+
+
+        val primer: DataPoint = DataPoint(sumarRestarDiasFecha(today,-4),0.0)
+        val segon: DataPoint = DataPoint(sumarRestarDiasFecha(today,-3),8.0)
+        val tercer: DataPoint = DataPoint(sumarRestarDiasFecha(today,-2),10.0)
+        val quart: DataPoint = DataPoint(sumarRestarDiasFecha(today,-1),32.0)
+        val cinc: DataPoint = DataPoint(today,args)
 
         val dp: Array<DataPoint> = arrayOf(primer,segon,tercer,quart,cinc)
 
         return dp;
 
     }
+
+    fun sumarRestarDiasFecha( fecha: Date, dias: Int): Date{
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.time = fecha
+        calendar.add(Calendar.DAY_OF_YEAR,dias)
+        print(calendar.time)
+        return calendar.time
+    }
+
+
 
 
 }

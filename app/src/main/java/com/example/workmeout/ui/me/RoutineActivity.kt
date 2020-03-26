3d6717b1dc194dee9c2d15e40d609e3c8e80259b
Ferran <ferrranespuna@gmail.com>
2020-03-26 19:10:07 +0100

    añadir días de entreno en las rutinas

---
+++ app/src/main/java/com/example/workmeout/ui/me/RoutineActivity.kt
@@ -0,0 +1,32 @@
+package com.example.workmeout.ui.me
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.workmeout.R
+import com.example.workmeout.data.ExerciseDataSourceDummy
+import com.example.workmeout.ui.sport.ExerciseAdapter
+
+class RoutineActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_routine)
+        var rv = findViewById<RecyclerView>(R.id.rv_1)
+        val sa = ExerciseAdapter()
+        sa.submitList(ExerciseDataSourceDummy.createDataSet())
+        rv.adapter = sa
+        rv.layoutManager = LinearLayoutManager(this)
+        var button1:Button = findViewById(R.id.dlt)
+
+    }
+
+    fun delete(v: View) {
+        Toast.makeText(this, "Delete this routine", Toast.LENGTH_SHORT).show()
+        finish()
+    }
+}

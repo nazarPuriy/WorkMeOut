455e5abeb1a1e228d4e0e7a64a535c6386ec5d3a
nachoMorera <ijmorera19@gmail.com>
2020-04-22 17:51:22 +0200

    Notifications

---
+++ app/src/main/java/com/example/workmeout/PushNotifications/ResultActivity.kt
@@ -0,0 +1,20 @@
+package com.example.workmeout.PushNotifications
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.workmeout.R
+import kotlinx.android.synthetic.main.activity_result.*
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        if (intent.getBooleanExtra("notification", false)) { //Just for confirmation
+            txtTitleView.text = intent.getStringExtra("title")
+            txtMsgView.text = intent.getStringExtra("message")
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/workmeout/chatPackage/model/User2.kt
@@ -0,0 +1,13 @@
+package com.example.workmeout.chatPackage.model
+
+data class User2 (
+    var name: String?,
+
+    var bio: String?,
+
+    var image: String?,
+
+    var last_message: String?
+) {
+
+}
